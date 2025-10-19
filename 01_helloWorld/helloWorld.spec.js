6e6a506bb0e49e07f66e3a9b482fc7f05e569e58
Brandon Rodriguez <brandon2932@gmail.com>
2025-10-18 20:40:08 -0700

    Add appropriate string to pass test

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,6 +1,6 @@
 const helloWorld = require('./helloWorld');
 
-describe('Hello World', function() {
+hdescribe('Hello World', function() {
   test('says "Hello, World!"', function() {
     expect(helloWorld()).toEqual('Hello, World!');
   });
