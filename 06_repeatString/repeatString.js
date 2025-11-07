2d9b85060b5969b2c06df9c941cb17b305c65df1
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-07 15:08:15 -0800

    Pass test cases

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) return "ERROR";
 
+  let arr = [];
+
+  while (arr.length < num) {
+    arr.push(string);
+  }
+
+  return arr.join('');
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
