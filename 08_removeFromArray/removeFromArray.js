50bd39e94b0d8e444c34537bff5d65d4e83dcbd5
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-07 18:07:13 -0800

    Add functionality to remove single value from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, val) {
+  const index = arr.indexOf(val);
+
+  arr.splice(index, 1);
+
+  return arr;
+
 };
+
+console.log(removeFromArray([1,2,3], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
