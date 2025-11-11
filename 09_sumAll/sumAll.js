ca048651709ed8a3ea7f091bf619d07f344109ff
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-11 15:14:12 -0800

    Add while loop to add numbers between to arguments

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  let count = 0;
 
+  while (num1 <= num2) {
+    count += num1;
+    num1++;
+  }
+
+  return count;
 };
+
+console.log(sumAll(1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
