4799afe1d2b0f74f1f599549188fe78ac834fcf0
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-11 15:26:02 -0800

    Return ERROR when negative arguments passed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,15 +1,20 @@
 const sumAll = function(num1, num2) {
-  let count = 0;
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (num1 > num2) [num1, num2] = [num2, num1];
+
+  // console.log(`num1 is ${num1}, num2 is ${num2}`)
+
+  let sum = 0;
 
   while (num1 <= num2) {
-    count += num1;
+    sum += num1;
     num1++;
   }
 
-  return count;
+  return sum;
 };
 
-console.log(sumAll(1, 4))
+console.log(sumAll(-1, 4))
 
 // Do not edit below this line
 module.exports = sumAll;
