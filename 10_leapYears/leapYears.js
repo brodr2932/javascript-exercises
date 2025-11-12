fea643c9d72245f8eeade0251859d8449706eb2a
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-12 13:39:08 -0800

    Add conditional logic to determine leap years

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if (!(year % 4 === 0)) {
+    console.log(`NOT a leap year!`);
+    return false; 
+  }
+
+  if (year % 4 === 0 && !(year % 100 === 0)) {
+    console.log(`It's a leap year!`);
+    return true;
+  } else if (year % 4 === 0 && year % 100 === 0) {
+    if (year % 400 === 0) {
+      console.log(`Its a leap year!`);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
 };
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
