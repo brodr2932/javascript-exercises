87cb8e5f11abddfc1edb539ab6afea45f11ea009
Brandon Rodriguez <brandon2932@gmail.com>
2025-11-12 13:40:37 -0800

    Remove console.log statements

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,22 +1,17 @@
 const leapYears = function(year) {
   if (!(year % 4 === 0)) {
-    console.log(`NOT a leap year!`);
     return false; 
   }
 
   if (year % 4 === 0 && !(year % 100 === 0)) {
-    console.log(`It's a leap year!`);
     return true;
   } else if (year % 4 === 0 && year % 100 === 0) {
     if (year % 400 === 0) {
-      console.log(`Its a leap year!`);
       return true;
     } else {
       return false;
     }
   }
-
-
 };
 
 console.log(leapYears(2000));
