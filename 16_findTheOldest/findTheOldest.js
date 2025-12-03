a40b73bf333112d0532b379424f8c0f9c1de3946
Brandon Rodriguez <brandon2932@gmail.com>
2025-12-03 15:21:19 -0800

    Use map and sort to sort ages in descending order

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,37 @@
-const findTheOldest = function() {
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function(arr) {
+  const arrWithAges = arr
+    .map(person => ({
+      name: person.name,
+      age: person.yearOfDeath - person.yearOfBirth
+    }))
+    .sort((a, b) => b.age - a.age);
+
+  return arrWithAges[0];
 };
+
+
+console.log(findTheOldest(people).name)
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
