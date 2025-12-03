98c1ad192a06fab7215e74cf6917181ac67c3f57
Brandon Rodriguez <brandon2932@gmail.com>
2025-12-02 19:08:58 -0800

    Complete multiply() function and pass tests

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -10,8 +10,8 @@ const sum = function(arr) {
 	return arr.reduce((acc, curr) => acc + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr);
 };
 
 const power = function() {
