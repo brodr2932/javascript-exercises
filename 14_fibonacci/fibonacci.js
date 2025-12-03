89098450e7e65ee3ba215e84b6fb9c54e3ace974
Brandon Rodriguez <brandon2932@gmail.com>
2025-12-03 13:21:20 -0800

    Generate first 25 numbers o Fibonacci sequence

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
 const fibonacci = function() {
 
+  let fibArray = [1, 1]; // starting Fibonacci sequence 
+  i = 0;
+
+  while (i < 23) { // generate the first 23 numbers in Fibonacci sequence
+    let sumOfPrevTwo = fibArray[i] + fibArray[i + 1];
+    fibArray.push(sumOfPrevTwo);
+    i++;
+  }
+
+  return fibArray;
 };
+
+
+console.table(fibonacci())
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
