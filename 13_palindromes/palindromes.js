8b26da40033f09d9e09a8890e21f646e3d637eb2
Brandon Rodriguez <brandon2932@gmail.com>
2025-12-02 21:26:21 -0800

    Add explanatory comments to palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,7 @@
 const palindromes = function (str) {
+  // Clean string to remove special characters and whitespaces
   const cleanedStr = str.replace(/[!,.?\s]/g, "").toLowerCase();
-
+  // Compare cleaned string to reverse of itself
   return cleanedStr === cleanedStr.split('').reverse().join('');
 };
 
