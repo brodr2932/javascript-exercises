f0a65563554432063419ed07f05eac23af59a52d
Brandon Rodriguez <brandon2932@gmail.com>
2025-12-02 21:21:22 -0800

    Implement palindrome detection with string cleaning and reverse comparison

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const cleanedStr = str.replace(/[!,.?\s]/g, "").toLowerCase();
 
+  return cleanedStr === cleanedStr.split('').reverse().join('');
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
