443a853e2c479ad4e5220906086026ed50c732b9
Brandon Rodriguez <brandon2932@gmail.com>
2025-12-04 13:47:26 -0800

    fix: calculate age for people without yearOfDeath

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,8 +1,7 @@
 const people = [
       {
         name: "Carly",
-        yearOfBirth: 1942,
-        yearOfDeath: 1970,
+        yearOfBirth: 2018,
       },
       {
         name: "Ray",
@@ -17,19 +16,23 @@ const people = [
     ]
 
 const findTheOldest = function(arr) {
+  const currentYear = new Date().getFullYear();
+
+  // Calculate age for every person and sort from oldest to youngest
   const arrWithAges = arr
-    .map(person => ({
-      name: person.name,
-      age: person.yearOfDeath - person.yearOfBirth
-    }))
+    .map(person => {
+      const deathYear = person.yearOfDeath || currentYear;
+      return {
+        name: person.name,
+        age: deathYear - person.yearOfBirth
+      };
+    })
     .sort((a, b) => b.age - a.age);
 
-  return arrWithAges[0];
+  return arrWithAges[0]; // return oldest person (first element in array)
 };
 
-
-console.log(findTheOldest(people).name)
-
+console.log(findTheOldest(people).name);
 
 
 
